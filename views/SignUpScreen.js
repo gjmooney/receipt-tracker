20e9a0db4325cf064154c93d29e80fd456c27b3a
Greg <gjmooney@asu.edu>
2023-01-27 14:53:52 +0100

    Add signup/login/welcome screens

--- views/SignUpScreen.js
+++ views/SignUpScreen.js
@@ -1,33 +1,21 @@
-/* import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-const auth = getAuth();
-
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
- */
-
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-ui.start("#firebaseui-auth-container", {
-  signInOptions: [
-    {
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
-    },
-  ],
-  // Other config options...
-});
-
-// Is there an email link sign-in?
-if (ui.isPendingRedirect()) {
-  ui.start('#firebaseui-auth-container', uiConfig);
+export default function SignUpScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>SignUp screen!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- views/WelcomeScreen.js
+++ views/WelcomeScreen.js
@@ -2,10 +2,10 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function LoginScreen() {
+export default function WelcomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Login screen!</Text>
+      <Text>Welcome screen!</Text>
       <StatusBar style="auto" />
     </View>
   );
